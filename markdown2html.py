590d54e71de2bdc44a5de5d807650296ebb2d46d
Diego Orejuela <dfrodriguezor@gmail.com>
2020-05-02 16:13:06 -0500

    add final changes about convertions on headings

--- markdown2html.py
+++ markdown2html.py
@@ -12,6 +12,8 @@ Requirements:
   <filename> and exit 1
 - Otherwise, print nothing and exit 0
 """
+
+
 def database():
 
     types = [
@@ -34,7 +36,7 @@ def database():
         "markdown": {
           "code": "#",
           "type": "at_beginning"
-        } ,
+        },
         "html_generated": {
           "tags": {
             "init": "<h1>",
@@ -47,7 +49,7 @@ def database():
         "markdown": {
           "code": "##",
           "type": "at_beginning"
-        } ,
+        },
         "html_generated": {
           "tags": {
             "init": "<h2>",
@@ -60,7 +62,7 @@ def database():
         "markdown": {
           "code": "###",
           "type": "at_beginning"
-        } ,
+        },
         "html_generated": {
           "tags": {
             "init": "<h3>",
@@ -73,7 +75,7 @@ def database():
         "markdown": {
           "code": "####",
           "type": "at_beginning"
-        } ,
+        },
         "html_generated": {
           "tags": {
             "init": "<h4>",
@@ -86,7 +88,7 @@ def database():
         "markdown": {
           "code": "#####",
           "type": "at_beginning"
-        } ,
+        },
         "html_generated": {
           "tags": {
             "init": "<h5>",
@@ -97,63 +99,112 @@ def database():
       }
     ]
 
-    return elements, types
+    return {"elements": elements, "types": types}
 
-def markdown2html(markdown_file, name_html_file):
-  # Database
-  db = database()
 
-  # Open file
-  file = open(markdown_file, "r")
+def generate_file_html(path_file, lines):
 
-  for line in file:
-    if line != "\n":
-      line_formattted = line.replace('\n', "")
-      html_parsed_line = parser_to_html(line_formattted, db)
-      print(html_parsed_line)
+    html_file = open(path_file, "w")
 
-def parser_to_html(string, database):
-  import re
+    for line in lines:
+        html_file.write(line)
 
-  #unpack database
-  elements, types = database
+    html_file.close()
 
-  string_parts = string.split(" ", 1)
-  convertions_queue = []
+    return
 
-  for type in types:
-    elements_to_search = filter(lambda element: element['markdown']["type"] == type["name"], elements)
-    
-    for element in elements_to_search:
-      #import pdb; pdb.set_trace()
-      if type["name"] == "at_beginning":
-        if string_parts[0] == element["markdown"]["code"]:
-          convertions_queue.append({
-            "element": element,
-            "match": None
-          })
-  
-  string_converted = string_parts[1]
-  for convertion in convertions_queue:
-    if convertion["match"]:
-      pass
-    else:
-      string_converted = convertion["element"]["html_generated"]["tags"]["init"] + string_converted + convertion["element"]["html_generated"]["tags"]["end"]
-    
-  return string_converted
 
+def converter_string_by_line(content, convertions_queue):
+    string_converted = content
+    for convertion in convertions_queue:
+        if convertion["match"]:
+            pass
+        else:
+            string_converted = "{}{}{}\n".format(
+              convertion["element"]["html_generated"]["tags"]["init"],
+              string_converted,
+              convertion["element"]["html_generated"]["tags"]["end"])
+
+    return string_converted
+
+
+def converter_markdown_at_beginning(init_string):
+
+    convertion = None
+    at_beginning_elements = filter(
+        lambda element: element['markdown']["type"] == "at_beginning",
+        database()["elements"]
+      )
+
+    for element in at_beginning_elements:
+        # import pdb; pdb.set_trace()
+        if init_string == element["markdown"]["code"]:
+            convertion = {
+              "element": element,
+              "match": None
+            }
+    return convertion
+
+
+def parser_to_html(string):
+
+    types = database()["types"]
+    string_parts = string.split(" ", 1)
+    content = string
+    convertions_queue = []
+
+    for type in types:
+        if type["name"] == "at_beginning":
+            convertion = converter_markdown_at_beginning(string_parts[0])
+            convertions_queue.append(convertion) if convertion else None
+            if len(convertions_queue) == 1:
+                content = string_parts[1] if len(string_parts) > 1 else ""
+
+        if content:
+            if type["name"] == "only_content":
+                pass
+                # convertions_queue = converter_markdown_only_content(content)
+            elif type["name"] == "in_content":
+                pass
+                # convertions_queue = converter_markdown_in_content(content)
+
+    string_converted = converter_string_by_line(content, convertions_queue)
+
+    return string_converted
+
+
+def markdown2html(markdown_file_path, html_file_path):
+
+    converted_lines = []
+
+    file = open(markdown_file_path, "r")
+
+    for line in file:
+        if line != "\n":
+            line_formattted = line.replace('\n', "")
+            html_parsed_line = parser_to_html(line_formattted)
+            converted_lines.append(html_parsed_line)
+
+    file.close()
+
+    generate_file_html(html_file_path, converted_lines)
+
+    return
+
+
+# Entry point
 if __name__ == "__main__":
     import sys
     from os import path
 
-    # Entry point
     if len(sys.argv) < 3:
         error_mssg = "Usage: ./markdown2html.py README.md README.html\n"
         sys.stderr.write(error_mssg)
         exit(1)
     else:
         if path.exists(sys.argv[1]) and path.isfile(sys.argv[1]):
-            markdown2html(sys.argv[1], sys.argv[2])
+            markdown_file_path, html_file_path = sys.argv[1], sys.argv[2]
+            markdown2html(markdown_file_path, html_file_path)
             exit(0)
         else:
             sys.stderr.write("Missing {}\n".format(sys.argv[1]))
